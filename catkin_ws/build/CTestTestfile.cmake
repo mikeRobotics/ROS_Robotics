cd0f7e38bebf06d062e20e659e52bdb09a4ac793
michaeltetteh <miketetteh751@outlook.com>
2020-08-02 13:49:55 +0000

    catkin workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/lazarus/Desktop/Robotics/ROS_Robotics/catkin_ws/src
+# Build directory: /home/lazarus/Desktop/Robotics/ROS_Robotics/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
