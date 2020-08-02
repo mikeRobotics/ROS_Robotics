5685b5237f9f00f52e5c84d1e82463d3c64aec06
michaeltetteh <miketetteh751@outlook.com>
2020-08-02 17:49:57 +0000

    differential drive robot urdf file added

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/lazarus/Desktop/Robotics/ROS_Robotics/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/lazarus/Desktop/Robotics/ROS_Robotics/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/lazarus/Desktop/Robotics/ROS_Robotics/catkin_ws/devel;/opt/ros/melodic' != '' else []
--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/lazarus/Desktop/Robotics/ROS_Robotics/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/lazarus/Desktop/Robotics/ROS_Robotics/catkin_ws/build/ros_robotics/cmake_install.cmake")
 
 endif()
 
